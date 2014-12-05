42b886dc0a8c30ff12cbb5b8085f858862615904
borde <borde@telecom-paristech.fr>
2014-12-05 17:07:51 +0100

    add functional code and rename functions accordingly in aadl models in order to produce executable code

---
+++ display-manager/functional-code/engine.c
@@ -0,0 +1,12 @@
+#include "engine.h"
+#include "gtypes.h"
+
+void simulate_engine(dm__legacy__types__engine_status * status)
+{
+}
+
+void engines_status_voter(dm__legacy__types__engine_status engine1,
+	dm__legacy__types__engine_status engine2,
+	dm__legacy__types__engines_status_i * status)
+{
+}
---
+++ display-manager/functional-code/gps.c
@@ -0,0 +1,10 @@
+#include "gps.h"
+#include "gtypes.h"
+
+void gps_simulate(dm__legacy__types__gps_position_i * pos)
+{
+}
+
+void gps_to_speed(dm__legacy__types__gps_position_i pos, dm__legacy__types__speed * speed)
+{
+}
