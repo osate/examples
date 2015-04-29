82ae41a0fb45a9e9d8b35456a226a6e2a3e0e45e
Julien <julien@gunnm.org>
2015-04-29 14:08:01 -0400

    Add new display code

--- core-examples/code-generation/roll-control/code/display.c
+++ core-examples/code-generation/roll-control/code/display.c
@@ -1,41 +1,39 @@
 #include "gtypes.h"
 #include "deployment.h"
 
+int nbiter = 0;
 
 void display 
     (roll_control__types__bool left_warning,
     roll_control__types__bool right_warning,
     roll_control__types__real roll_rate)
 {
-//	myprint_double ("Roll Rate", myprint_double);
-	if (left_warning == 1)
-	{
-#ifdef OCARINA_RUNTIME_DEOS
-		myprint_str ("LEFT WARNING ON");
-#endif
-	}
-	else
-	{
-#ifdef OCARINA_RUNTIME_DEOS
-		myprint_str ("Left Warning Off");
-#endif
-	}
-
-	if (right_warning == 1)
-	{
-#ifdef OCARINA_RUNTIME_DEOS
-		myprint_str ("RIGHT WARNING ON");
-#endif
-	}
-	else
-	{
-#ifdef OCARINA_RUNTIME_DEOS
-		myprint_str ("Right Warning Off");
-#endif
-	}
+//	myprint_double ("Roll Rate", amyprint_double);
 
 #ifdef OCARINA_RUNTIME_DEOS
-	myprint_int ("Roll Rate", (int)roll_rate);
+
+	if ( (left_warning == 1) && (right_warning == 1))
+	{
+		myprint_str_int ("LEFT WARNING ON  | RIGHT WARNING ON ", nbiter);
+	}
+	if ( (left_warning == 1) && (right_warning == 0))
+	{
+		myprint_str_int ("LEFT WARNING ON  | RIGHT WARNING OFF ", nbiter);
+	}
+	if ( (left_warning == 0) && (right_warning == 1))
+	{
+		myprint_str_int ("LEFT WARNING OFF | RIGHT WARNING ON ", nbiter);
+	}
+	if ( (left_warning == 0) && (right_warning == 0))
+	{
+		myprint_str_int ("LEFT WARNING OFF | RIGHT WARNING OFF ", nbiter);
+	}
+nbiter++;
 #endif
+
+
+//#ifdef OCARINA_RUNTIME_DEOS
+//	myprint_int ("Roll Rate", (int)roll_rate);
+//#endif
 
 }
