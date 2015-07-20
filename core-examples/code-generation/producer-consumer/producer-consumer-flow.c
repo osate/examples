c1d3892312712ee56c11770884c513be70ffc3f4
Julien <julien@gunnm.org>
2015-07-20 14:41:11 -0400

    Add producer-consumer example

---
+++ core-examples/code-generation/producer-consumer/producer-consumer-flow.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+#include <po_hi_time.h>
+
+int produce_data = 0;
+
+void user_produceflow_spg (int* data)
+{
+#if defined (POSIX)
+   struct timespec mytime;
+   clock_gettime (CLOCK_REALTIME, &mytime);
+   *data = produce_data;
+   printf ("At time %3lu:%3lu, produce : %d\n", mytime.tv_sec % 3600, mytime.tv_nsec/1000000,produce_data);
+#else
+   printf ("Produce %d\n", data);
+#endif
+   produce_data++;
+}
+
+void user_consumeflow_spg (int data)
+{
+#if defined (POSIX)
+   struct timespec mytime;
+   clock_gettime (CLOCK_REALTIME, &mytime);
+   printf( "At time %3lu:%3lu, consume : %d\n", mytime.tv_sec % 3600 , mytime.tv_nsec/1000000,data);
+#else
+   printf ("Consume %d\n", data);
+#endif
+
+}
+
+
+}
---
+++ core-examples/code-generation/producer-consumer/producer-consumer-var.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+#include <types.h>
+#include <po_hi_time.h>
+
+int produce_data = 0;
+
+extern software__alpha_type shareddata;
+
+void user_producevar_spg ()
+{
+#if defined (POSIX)
+   struct timespec mytime;
+   clock_gettime (CLOCK_REALTIME, &mytime);
+   shareddata = produce_data;
+   printf ("At time %3lu:%3lu, produce : %d\n", mytime.tv_sec % 3600, mytime.tv_nsec/1000000,produce_data);
+#else
+   printf ("Produce %d\n", data);
+#endif
+   produce_data++;
+}
+
+void user_consumevar_spg ()
+{
+#if defined (POSIX)
+   struct timespec mytime;
+   clock_gettime (CLOCK_REALTIME, &mytime);
+   printf( "At time %3lu:%3lu, consume : %d\n", mytime.tv_sec % 3600 , mytime.tv_nsec/1000000,shareddata);
+#else
+   printf ("Consume %d\n", data);
+#endif
+
+}
