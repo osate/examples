b9c06ab330b4ef59a84e085c8f22768e944cb88a
Julien <julien@gunnm.org>
2015-07-20 15:48:02 -0400

    Add task synchronization models

---
+++ core-examples/code-generation/task-sync/tasksync.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <time.h>
+#include <po_hi_time.h>
+
+int produce_data = 0;
+
+void user_produceflow_spg (int* data)
+{
+#if defined (POSIX)
+   struct timespec mytime;
+   clock_gettime (CLOCK_REALTIME, &mytime);
+   *data = produce_data;
+   printf ("At time %3lu:%3lu, produce : %d\n", mytime.tv_sec % 3600, mytime.tv_nsec/1000000,produce_data);
+#else
+   printf ("Produce %d\n", data);
+#endif
+   produce_data++;
+}
+
+void user_consumeflow_spg (int data)
+{
+#if defined (POSIX)
+   struct timespec mytime;
+   clock_gettime (CLOCK_REALTIME, &mytime);
+   printf( "At time %3lu:%3lu, consume : %d\n", mytime.tv_sec % 3600 , mytime.tv_nsec/1000000,data);
+#else
+   printf ("Consume %d\n", data);
+#endif
+
+}
+
+
+void user_computeflow_spg (int *dataout, int datain)
+{
+#if defined (POSIX)
+   struct timespec mytime;
+   *dataout = 2 * datain;
+   clock_gettime (CLOCK_REALTIME, &mytime);
+   printf( "At time %3lu:%3lu, compute : %d\n", mytime.tv_sec % 3600 , mytime.tv_nsec/1000000,*dataout);
+#else
+   printf ("Compute %d\n", data);
+#endif
+
+}
+
+
+
